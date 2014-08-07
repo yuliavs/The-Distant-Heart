1aec725619741e99e2913db0258f6669e37dea1a
Paul Gesiak <g33.paul@gmail.com>
2014-08-07 16:55:14 +0100

    various changes

--- get_log.sh
+++ get_log.sh
@@ -1,1 +1,2 @@
-ssh ys 'cat /home/ubuntu/yulias_necklaces/data/log.json' | sed -E 's/^[^ ]+ //' 
+# ssh ys 'cat /home/ubuntu/yulias_necklaces/data/log.json' | sed -E 's/^[^ ]+ //'
+ssh ys 'cat /home/ubuntu/yulias_necklaces/data.log' | sed -E 's/^[^ ]+ //' 
---
+++ setup.sh
@@ -0,0 +1,8 @@
+pip install PyYaml
+pip install requests
+pip install BeautifulSoup4
+git clone https://github.com/Zulko/twittcher.git
+cd twittcher
+python setup.py install
+cd ..
+rm twittcher
